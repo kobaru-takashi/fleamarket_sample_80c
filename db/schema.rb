fa7b21210b48cb89d601b7d0a95ee9ab0f69abde
yusuke0621 <yusuke19930621@gmail.com>
2020-08-06 18:46:57 +0900

    show.html.haml手直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_062808) do
+ActiveRecord::Schema.define(version: 2020_08_06_053102) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -18,8 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_062808) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
-
-ActiveRecord::Schema.define(version: 2020_08_06_053102) do
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
