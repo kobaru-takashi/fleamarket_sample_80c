e68b8ec97eb4ed8000e572c424afdf31589f208a
yusuke0621 <yusuke19930621@gmail.com>
2020-08-06 18:10:04 +0900

    category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,24 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @category_parent_array = Category.where(ancestry: nil)
   end
 
   def create
   end
 
   def show
+    @category_id = @product.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
+  end
+
+  def get_category_children
+    @category_children = Category.find("#{params[:parent_id]}").children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'products#index'
+  resources :products do
+    collection do
+      get 'get_category_children', defaults: { fomat: 'json'}
+      get 'get_category_grandchildren', defaults: { fomat: 'json'}
+      get 'search'
+      get 'post_done'
+      get 'delete_done'
+      get 'detail_search'
+      get 'update_done'
+    end
+  end
 end
