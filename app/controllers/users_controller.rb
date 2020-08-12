f61d23c85558dd51f4ee836a971ce97eccf3d126
mocomoco12 <addm3369@gmail.com>
2020-08-12 11:05:31 +0900

    ログアウト追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show]
   def edit
+    @user = User.find_by(id: params[:id])
   end
 
   def update
@@ -11,14 +12,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+
   def show
-    @user = User.where(user_id: current_user.id)
+    unless @user == current_user
+      redirect_to root_path(@user)
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:email, :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 
 end
