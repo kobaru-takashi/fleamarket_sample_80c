db570e9ecfe7f5325532c30b08a08a3b5d052702
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-08-13 11:33:36 +0900

    conflict 解消

--- app/assets/javascripts/modules/category.js
+++ app/assets/javascripts/modules/category.js
@@ -1,85 +1,48 @@
-$(document).on('turbolinks:load', function(){
-
+$(function(){
   $(function(){
-    function appendOption(category){
-      var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
-      return html;
-    }
-    function appendChildrenBox(insertHTML){
-      var childSelectHtml = '';
-      childSelectHtml = `<div class='listing-select-wrapper__added' id= 'children_wrapper'>
-      <div class='listing-select-wrapper__box'>
-      <select class="listing-select-wrapper__box--select" id="child_category" name="product[category_id]">
-      <option value="" data-category="">選択してください</option>
-      ${insertHTML}
-      <select>
-      </div>
-      </div>`;
-      $('.listing-product-detail__category').append(childSelectHtml);
-    }
-    function appendGrandchildrenBox(insertHTML){
-      var grandchildSelectHtml = '';
-      grandchildSelectHtml = `<div class='listing-select-wrapper__added' id= 'grandchildren_wrapper'>
-      <div class='listing-select-wrapper__box'>
-      <select class="listing-select-wrapper__box--select" id="grandchild_category" name="product[category_id]">
-      <option value="" data-category="">選択してください</option>
-      ${insertHTML}
-      <select>
-      </div>
-      </div>`;
-      $('.listing-product-detail__category').append(grandchildSelectHtml);
-    }
-    $('#parent_category').on('change', function(){
-      var parentCategory = document.getElementById('parent_category').value;
-      if (parentCategory != ""){
-        $.ajax({
-          url: '/products/get_category_children',
-          type: 'GET',
-          data: { parent_id: parentCategory },
-          dataType: 'json'
-        })
-        .done(function(children){
-          $('#children_wrapper').remove();
-          $('#grandchildren_wrapper').remove();
-          var insertHTML = '';
-          children.forEach(function(child){
-            insertHTML += appendOption(child);
-          });
-          appendChildrenBox(insertHTML);
-        })
-        .fail(function(){
-          alert('カテゴリー取得に失敗しました');
-        })
-      }else{
-        $('#children_wrapper').remove();
-        $('#grandchildren_wrapper').remove();
-      }
+
+    $('.box__parent--link').mouseover(function(){
+      $(this).css("background-color", "#3ccace").css("color", "white");
+    }).mouseout(function(){
+      $(this).css("background-color", "white").css("color", "black");
     });
-    $('.listing-product-detail__category').on('change', '#child_category', function(){
-      var childId = $('#child_category option:selected').data('category');
-      if (childId != ""){
-        $.ajax({
-          url: '/products/get_category_grandchildren',
-          type: 'GET',
-          data: { child_id: childId },
-          dataType: 'json'
-        })
-        .done(function(grandchildren){
-          if (grandchildren.length != 0) {
-            $('#grandchildren_wrapper').remove();
-          var insertHTML = '';
-          grandchildren.forEach(function(grandchild){
-            insertHTML += appendOption(grandchild);
-          });
-          appendGrandchildrenBox(insertHTML);
-        }
-      })
-      .fail(function(){
-        alert('カテゴリー取得に失敗しました');
-      })
-    }else{
-      $('#grandchildren_wrapper').remove();
-    }
+  $('a[href^="#"]').click(function() {
+    var speed = 400; 
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+    })
   });
 });
-});
+
+$(function(){
+  $('.category__parent--name').mouseover(function(){
+    $(this).css("background-color", "#3ccace").css("color", "white");
+  }).mouseout(function(){
+    $(this).css("background-color", "white").css("color", "black");
+  });
+  $('.category__child--name').mouseover(function(){
+    let parent = $(this).parent().parent().parent().children('a');
+    $(this).css("background-color", "lightgray");
+    parent.css("background-color", "#3ccace").css("color", "white");
+  }).mouseout(function(){
+    let parent = $(this).parent().parent().parent().children('a');
+    $(this).css("background-color", "white").css("color", "black");
+    parent.css("background-color", "#3ccace").css("color", "white");
+  });
+  $('.category__grandchild--name').mouseover(function(){
+    let child  = $(this).parent().parent().parent().children('a');
+    let parent = child.parent().parent().parent().children('a');
+    $(this).css("background-color", "lightgray")
+    child.css("background-color", "lightgray");
+    parent.css("background-color", "#3ccace").css("color", "white");
+  }).mouseout(function(){
+    let child  = $(this).parent().parent().parent().children('a');
+    let parent = child.parent().parent().parent().children('a');
+    $(this).css("background-color", "white").css("color","black");
+    child.css("background-color", "white").css("color","black");
+    parent.css("background-color", "white").css("color","black");
+  });
+})
