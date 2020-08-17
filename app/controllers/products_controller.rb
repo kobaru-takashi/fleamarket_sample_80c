9bc52d7f96a799cb37a94d5b916a951f88000df1
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-08-17 12:53:56 +0900

    商品詳細ページ
    出品中と売却済の条件分岐

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to product_path(params[:id])
+      redirect_to root_path
     else
       render new_product_path
     end
