449f0ad4fd8b248ab69a07f9904c620f43adb491
kobaru-takashi <ta.a.1445.1@gmail.com>
2020-08-20 15:01:59 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :destroy]
   before_action :authenticate_user!, only: [:show]
-  before_action :set_parents, only: [:show]
+  before_action :set_parents, only: [:show, :likes]
+
 
   def destroy
     user.destroy
