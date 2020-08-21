894cd6ef1181953dc5aef1edc4e2a42b2d3eafcb
f4rk19 <67687475+f4rk19@users.noreply.github.com>
2020-08-21 14:53:25 +0900

    Update 20200806034457_create_products.rb

--- db/migrate/20200806034457_create_products.rb
+++ db/migrate/20200806034457_create_products.rb
@@ -11,7 +11,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.string :brand, null:false
       t.references :user
       t.references :category
-      t.integer :delivery_date, null:false
+      t.timestamps
     end
   end
 end
